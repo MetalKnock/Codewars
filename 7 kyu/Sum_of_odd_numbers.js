a3d2d367f95a3da8c368298865bdd2dec123fa25
MetalKnock <dimannisik1@gmail.com>
2022-07-26 22:33:09 +0700

    modified:   7 kyu/Sum_of_odd_numbers.js

--- 7 kyu/Sum_of_odd_numbers.js
+++ 7 kyu/Sum_of_odd_numbers.js
@@ -11,7 +11,7 @@ Given the triangle of consecutive odd numbers:
 Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 
 1 -->  1
-2 --> 3 + 5 = 8
+2 --> 3 + 5 = 8 
 
 
 */
